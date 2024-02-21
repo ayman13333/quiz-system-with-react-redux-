c1da43c11d596c170818a2189fc920f35874fa64
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-21 15:33:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch=useDispatch();
+  const reducer =useSelector(state=>state.choices);
+
+  console.log(reducer);
   return (
    <h1>done</h1>
   );
