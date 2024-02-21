f66f713ebbf692f7b4f6cd6a96c73926be57135a
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-22 01:37:53 +0200

    update

--- src/pages/UserResultsPage.js
+++ src/pages/UserResultsPage.js
@@ -1,34 +1,41 @@
 import { useSelector } from "react-redux";
 
 export default function UserResultsPage() {
-    const { userAnswers , totalScore } = useSelector(
-        (state) => state.choices
-      );
+  const { userAnswers, totalScore } = useSelector((state) => state.choices);
 
-      console.log(totalScore);
+  console.log(totalScore);
 
   return (
     <div className="mx-auto my-auto">
-        <div className="d-flex justify-content-between">
-            <h4>Your Results</h4>
-        <h4>TotalScore :<span className="mx-2">{`${totalScore} %`}</span> </h4>
-        </div>
-        
-        { userAnswers.map((answer,i)=>(
-            <div key={i}>
-             <h3  className="my-4 w-200">{answer.question}</h3>
-             <div className="row">
-                {
-                    answer.choices.map((c,index)=>(
-                        <div key={index} className={`col-sm-2 answer mx-1 p-2 ${answer.correctAnswer==c ? 'correctAnswer' : ''} ${(answer.correctAnswer!=answer.userAnswer)&&(answer.userAnswer==c) ? 'wrongAnswer' : ''}`}>{c}</div>
-                    ))
-                }
-               
-             </div>
-             </div>
-        ))}
+      <div className="d-flex justify-content-between">
+        <h4>Your Results</h4>
+        <h4>
+          TotalScore :<span className="mx-2">{`${totalScore} %`}</span>{" "}
+        </h4>
+      </div>
 
-   
-  </div>
-  )
+      {userAnswers.map((answer, i) => (
+        <div key={i}>
+          <h3 className="my-4 w-200">{answer.question}</h3>
+          <div className="row">
+            {answer.choices.map((c, index) => (
+              <div
+                key={index}
+                className={`col-sm-2 answer mx-1 p-2 ${
+                  answer.correctAnswer == c ? "correctAnswer" : ""
+                } ${
+                  answer.correctAnswer != answer.userAnswer &&
+                  answer.userAnswer == c
+                    ? "wrongAnswer"
+                    : ""
+                }`}
+              >
+                {c}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
